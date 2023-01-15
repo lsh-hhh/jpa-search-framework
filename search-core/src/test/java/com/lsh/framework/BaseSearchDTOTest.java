1591722324833b8a38176aef68a8be680c71757c
linsihang <lshandhsl@outlook.com>
2023-01-16 00:15:52 +0800

    测试

---
+++ search-core/src/test/java/com/lsh/framework/BaseSearchDTOTest.java
@@ -0,0 +1,13 @@
+package com.lsh.framework;
+
+import org.junit.jupiter.api.Test;
+
+class BaseSearchDTOTest {
+
+    @Test
+    void getFixedWhere() {
+
+        BaseSearchDTO baseSearchDTO = new BaseSearchDTO();
+        System.out.println(baseSearchDTO.getPage());
+    }
+}
